75aa02cfcc50d03c257050c9d9cb503f4edfed5c
unknown <barrioss.vanesa@gmail.com>
2022-08-15 04:33:40 -0300

    se modifica app

--- app.js
+++ app.js
@@ -8,5 +8,12 @@ app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "./views/home.html"))
 })
 
+app.get("/register", (req, res) =>{
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+});
+
+app.get("/login", (req, res) =>{
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+});
 
 app.listen(3060, () => console.log("Servidor corriendo en el puerto 3060"));
