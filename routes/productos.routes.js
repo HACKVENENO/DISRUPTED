1fc731d2339f31b596ab2dd875a65d1a17a19a82
Candelaria Sanchez <sanchezgcandelaria@gmail.com>
2022-09-01 23:31:24 -0300

    principio multer

--- routes/productos.routes.js
+++ routes/productos.routes.js
@@ -3,18 +3,33 @@ const productosController = require('../controllers/productos.controller');
 const router = express.Router();
 
 
+// ************ MULTER ************
+const multer = require("multer");
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/images/products");
+  },
+  filename: function (req, file, cb) {
+    console.log({ file });
+
+    // cb(null, file.fieldname + "-" + Date.now());
+    cb(null, Date.now() + "" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 router.get('/', productosController.productos);
-//router.get("/details", productosController.details);
+
 router.get("/details/:id", productosController.details);
 
-router.post('/create', productosController.create);
+router.get('/create', productosController.create);
+router.post('/create', upload.single("fotoProducto"), productosController.store);
 
-router.get('/store', productosController.store);
 
 router.get('/edit/:idProducto', productosController.edit);
-router.put('/edit/:idProducto', productosController.edit);
-
-router.put('/:id', productosController.update);
+router.put('/edit/:idProducto', productosController.update);
 
 router.delete('/destroy', productosController.destroy);
 
