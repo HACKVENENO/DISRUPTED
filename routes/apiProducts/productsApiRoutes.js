89e254bb511df6302bb7f4c490e431126d16780d
Candelaria Sanchez <sanchezgcandelaria@gmail.com>
2022-12-03 02:01:18 -0300

    casi funca la api productos tambien

--- routes/apiProducts/productsApiRoutes.js
+++ routes/apiProducts/productsApiRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const apiProductController = require('../../controllers/apiProducts/productsApiController');
 
 router.get('/products', apiProductController.productos);
+router.get('/products/:id', apiProductController.detalle);
 
 module.exports = router;
